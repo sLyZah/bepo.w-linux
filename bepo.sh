da88f4a72627dbe2ebb1053e0718f66db38f10dd
Sylvain M <sLyZah@users.noreply.github.com>
2020-10-27 22:44:54 +0100

    Add files via upload

--- bepo.sh
+++ bepo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo cp Downloads/evdev.xml /usr/share/X11/xkb/rules/evdev.xml > echo "evdev.xml has been replaced"
-sudo cp Downloads/fr /usr/share/X11/xkb/symbols/fr > echo "symbols/fr has been replaced"
+sudo mv Downloads/evdev.xml /usr/share/X11/xkb/rules/evdev.xml > echo "evdev.xml has been replaced"
+sudo mv Downloads/fr /usr/share/X11/xkb/symbols/fr > echo "symbols/fr has been replaced"
 echo "Select the correct keyboard language >> French (Bepo, Intl, Flamme Sly)"
