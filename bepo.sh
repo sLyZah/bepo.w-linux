89032950d86c0b7d1898855b24d5af5cbd52ba38
Sylvain M <sLyZah@users.noreply.github.com>
2020-10-27 23:13:13 +0100

    Delete bepo.sh

--- bepo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo mv Downloads/evdev.xml /usr/share/X11/xkb/rules/evdev.xml
-sudo mv Downloads/fr /usr/share/X11/xkb/symbols/fr
-echo "Select the correct keyboard language >> French (Bepo, Intl, Flamme Sly)"
-echo ":)"
