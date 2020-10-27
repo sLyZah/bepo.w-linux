6211ce44bec74ef62a29c8d32c08b8a3241dbc6e
Sylvain M <sLyZah@users.noreply.github.com>
2020-10-27 22:49:13 +0100

    Update bepo.sh

--- bepo.sh
+++ bepo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-sudo mv Downloads/evdev.xml /usr/share/X11/xkb/rules/evdev.xml > echo "evdev.xml has been replaced"
-sudo mv Downloads/fr /usr/share/X11/xkb/symbols/fr > echo "symbols/fr has been replaced"
+sudo mv Downloads/evdev.xml /usr/share/X11/xkb/rules/evdev.xml
+sudo mv Downloads/fr /usr/share/X11/xkb/symbols/fr
 echo "Select the correct keyboard language >> French (Bepo, Intl, Flamme Sly)"
+echo ":)"
