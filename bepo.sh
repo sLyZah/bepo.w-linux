891adb7eabc10a29f825ce530b0b5e87d310c154
Sylvain M <sLyZah@users.noreply.github.com>
2020-10-27 22:38:04 +0100

    a scrit to replace evdev.xml and the fr file
    both files have to be in your Downloads folder to work.

---
+++ bepo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo cp Downloads/evdev.xml /usr/share/X11/xkb/rules/evdev.xml > echo "evdev.xml has been replaced"
+sudo cp Downloads/fr /usr/share/X11/xkb/symbols/fr > echo "symbols/fr has been replaced"
+echo "Select the correct keyboard language >> French (Bepo, Intl, Flamme Sly)"
